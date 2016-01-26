8a4abcfb4dba73bfbe34da21b7705047802aba12
Andres Guachisaca <caguachisaca@gmail.com>
2016-01-26 18:34:29 -0500

    cambios

--- includes/menuizquierda_admin.php
+++ includes/menuizquierda_admin.php
@@ -1,4 +1,4 @@
  <ul class="nav">
       <li><a href="slider_lista.php">Slider Principal</a></li>
-      <li><a href="slider_video.php">Slider Videos</a></li>
+      <li><a href="video_lista.php">Slider Videos</a></li>
  </ul>
