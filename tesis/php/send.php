2fe8e51856608048bccecd81c74f316c702a08a2
Andres Guachisaca <caguachisaca@gmail.com>
2016-02-20 13:40:51 -0500

    cambios en envio de correo

--- tesis/php/send.php
+++ tesis/php/send.php
@@ -2,7 +2,7 @@
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
 $mensaje = $_POST['mensaje'];
-$para = 'pinasarqueologico@gmail.com';
+$para = 'admin@pinasarqueologico.com';
 $titulo = 'MENSAJE DESDE PIÑAS ARQUEOLOGICO';
 $header = 'From: ' . $email;
 $msjCorreo = "Nombre: $nombre\n E-Mail: $email\n Mensaje:\n $mensaje";
@@ -11,7 +11,7 @@ if ($_POST['submit']) {
 if (mail($para, $titulo, $msjCorreo, $header)) {
 echo "<script language='javascript'>
 alert('Su mensaje se envio correctamente.');
-window.location.href = 'http://localhost/MunicipioPinas/tesis/contact.php';
+window.location.href = 'http://www.pinasarqueologico.com/tesis/contact.php';
 </script>";
 } else {
   echo 'Fallo el envio';
